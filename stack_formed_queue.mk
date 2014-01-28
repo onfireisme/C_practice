c9bad970ee3093b8f8186387035871478e56e066
onfireisme <onfireisme@gmail.com>
2014-01-28 16:36:56 +0800

    reoginize all the files

--- stack_formed_queue.mk
+++
@@ -1,8 +0,0 @@
-stack_formed_queue:two_stack_form_one_queue.o stack.o
-	cc -o stack_formed_queue two_stack_form_one_queue.o stack.o
-two_stack_form_one_queue.o:two_stack_form_one_queue.c two_stack_form_one_queue.h 
-	cc -c two_stack_form_one_queue.c
-stack.o:stack.c stack.h
-	cc -c stack.c
-clean:
-	rm stack_formed_queue stack.o two_stack_form_one_queue.o
--- string_rotate_check.h
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<malloc.h>
-#define false 0
-#define true 1
-int is_include_rotate_string(char *compared_string,
-		char *compare_string,int compare_string_length,int compared_string_length);
-char * double_copy_string(char *source_string,int string_length);
